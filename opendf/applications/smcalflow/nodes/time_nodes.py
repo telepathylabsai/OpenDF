20f229bed7af140af08bd9d68c156e53b6822738
hankcs <hankcs@amazon.com>
2022-06-21 16:08:57 -0400

    Fix Unexpected input name pos1 to node HourMilitary

--- opendf/applications/smcalflow/nodes/time_nodes.py
+++ opendf/applications/smcalflow/nodes/time_nodes.py
@@ -1372,6 +1372,7 @@ class HourMilitary(Node):
 
     def __init__(self):
         super().__init__(Time)
+        self.signature.add_sig('pos1', Int, alias='hours')
         self.signature.add_sig('hours', Int, True)
 
     def exec(self, all_nodes=None, goals=None):
