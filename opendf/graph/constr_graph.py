1074bf2739ac0499be2f756c4c09e8c746371012
Joram Meron <joram.meron@telepathy.ai>
2022-10-31 13:08:21 +0100

    construct - check balanced brackets

--- opendf/graph/constr_graph.py
+++ opendf/graph/constr_graph.py
@@ -89,7 +89,8 @@ def construct_graph(sexp, d_context, register=True, top_only=False, constr_tag=R
         prs = parse_p_expressions(sexp)
     except Exception as ex:
         if sexp and sexp.count('(')!=sexp.count(')'):
-            print('\n> > >Unbalanced brackets:\n\n' + indent_sexp(sexp, sep_brack=True) + '\n')
+            print('\n> > >Unbalanced brackets: #open - #close = %d\n\n'%
+                  (sexp.count('(')!=sexp.count(')')) + indent_sexp(sexp, sep_brack=True) + '\n')
         re_raise_exc(ex)
 
     try:
