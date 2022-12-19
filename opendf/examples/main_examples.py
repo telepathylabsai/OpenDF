769dec9d793cbb2337672de65a6ada29ad88e84f
Victor Guimarães <victor.guimaraes@telepathy.ai>
2022-12-19 14:59:02 +0100

    Updated examples.

--- opendf/examples/main_examples.py
+++ opendf/examples/main_examples.py
@@ -232,40 +232,63 @@ dialogs = [
         # 'refer(Recipient?(name=AND(LIKE(PersonName(John)),NOT(LIKE(PersonName(Doe))))))',
     ],
 
-    ['Int()'],  # 1
-    ['Int(10)'],  # 2
-
+    # 1
+    [
+        'DeleteEvent(AND(starts_at(Tomorrow()), with_attendee(FindManager(John))))'
+    ],
+    # 2
+    [
+        # 'CreateEvent(starts_at(Morning()))',
+        'CreateEvent(starts_at(NumberAM(9)))',
+        'ModifyEventRequest(with_attendee(dan))',
+        # 'AcceptSuggestion(2)',
+        # 'AcceptSuggestion()',
+    ],
     # 3
     [
-        # 'Bool(yes)',
-        'Bool(Yes)',
-        # 'Bool(True)',
+        'UpdateEvent(AND(ends_at(HourMinuteAm(hours=10,minutes=30)), at_location(jeffs),\
+                    starts_at(NextDOW(SUNDAY)),starts_at(NumberAM(10))), \
+                    constraint=AND(ends_at(NumberPM(2)),starts_at(NumberAM(11))))',
     ],
-
-    # 4 - dialog #5 - weather, yield
+    # 4
     [
-        # 'IsCold(table=WeatherQueryApi(place=#zurich, time=DateTime?(date=Today(), time=Morning())))',
-        # 'Yield(output=WillSnow(table=WeatherQueryApi(place=#zurich, time=DateTime?(date=Today(), time=Morning()))))',
-        'Yield(output=WillSnow(place=zurich, time=DateTime?(date=Today(), time=Morning())))',
+        'UpdateEvent(event=has_id(4), constraint=starts_at(Time(hour=19)))',
     ],
-    # 5 - yield getattr / property
+    # 5
     [
-        # 'Yield(output=:day(Today()))',
-        # 'Yield(output=:start(Event(start=DateTime(date=Tomorrow(), time=Time(hour=8)))))',
-        'Yield(output=:start(Event(subject=party, start=DateTime(date=Tomorrow(), time=Time(hour=8)))))',
-        # 'Yield(output=:end(Event(subject=party, end=Tomorrow(), start=DateTime(date=Today(), time=Time(hour=8)))))',
+        'FindEvents(starts_at(Morning()))',
+        'ModifyEventRequest(starts_at(GT(Time(hour=9, minute=0))))',
     ],
-    # 6 - alias
+    # 6
     [
-        'Yield(output=Date(year=2021))',
-        'revise(hasParam=output, new=2020, newMode=extend)',
+        'FindEvents(AND(avoid_start(Morning()), at_location(room3)))',
     ],
-    # 7 - size (unroll), operator-function
+    # 7
     [
-        'size(SET(Time(hour=1),Time(hour=1),SET(Time(hour=2), Time(hour=2))))',
-        'size(SET(Time(hour=1),Time(hour=1),SET(Time(hour=2), Time(hour=2))), unroll=True)',
-        'size(Time(hour=1))',
-        'GTf(size(SET(Time(hour=1),Time(hour=1),SET(Time(hour=2), Time(hour=2)))), 1)',
-    ]
+        'WeatherQueryApi(place=AtPlace(place=FindPlace(Zurich)), time=Today())',
+        'WillSnow(table=refer(WeatherTable?()))',
+    ],
+    # 8
+    [
+        'FindEvents(Event?(slot=TimeSlot(bound=DateTimeRange('
+        'start=DateTime(date=Date(year=2022, month=1, day=5), time=Time(hour=10, minute=0)), '
+        'end=DateTime(date=Date(year=2022, month=1, day=6), time=Time(hour=13, minute=0))'
+        '))))',
+    ],
+    # 9
+    [
+        'FindEvents(Event?(slot=TimeSlot(inter=DateTimeRange('
+        'start=DateTime(date=Date(year=2022, month=1, day=5), time=Time(hour=10, minute=0)), '
+        'end=DateTime(date=Date(year=2022, month=1, day=6), time=Time(hour=13, minute=0))'
+        '))))',
+    ],
+    # 10
+    [
+        'GTf(size(FindEvents(with_attendee(jack))), Int(1))'
+    ],
+    # 11
+    [
+        'GTf(size(FindEvents(starts_at(GT(singleton(FindEvents(with_attendee(jack))))))), Int(1))',
+    ],
 
 ]
